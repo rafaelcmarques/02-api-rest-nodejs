8d965b27a8da1e5fd79d8abf99b88c26666bd7e6
Rafael Marques <rafaeldecarvalhomarques@gmail.com>
2023-07-06 22:22:40 -0300

    criado testes

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,5 @@
-import fastify from 'fastify'
 import { env } from './env'
-import cookie from '@fastify/cookie'
-import { transactionsRoute } from './routes/transactions'
-
-const app = fastify()
-
-app.addHook('preHandler', async (request) => {
-  console.log(`[${request.method}] ${request.url}`)
-})
-
-app.register(cookie)
-app.register(transactionsRoute, {
-  prefix: 'transactions',
-})
+import { app } from './app'
 
 app
   .listen({
